2cfd4c3fa0d5ea2a4b1126a527ed705ef0d4e311
Maggie Leavell <mjleavell@gmail.com>
2018-11-10 20:33:54 -0600

    WIP

--- src/components/legoCharacter/legoCharacter.js
+++ src/components/legoCharacter/legoCharacter.js
@@ -1,11 +1,12 @@
 import $ from 'jquery';
 import partsData from '../../data/partsData';
+import './legoCharacter.scss';
 
 const writeSelectedHead = (clickedHead) => {
   let domString = '';
   domString += `
-    <div id='${clickedHead.id}' class='card col-3 p-1 m-2'>
-      <img class='card-img-top' src=${clickedHead.imageUrl}
+    <div id='${clickedHead.id}' class='card'>
+      <img class='card-img' src=${clickedHead.imageUrl}
     </div>`;
   $('#head-name').html(clickedHead.name);
   $('#head').html(domString);
@@ -14,8 +15,8 @@ const writeSelectedHead = (clickedHead) => {
 const writeSelectedTorso = (clickedTorso) => {
   let domString = '';
   domString += `
-    <div id='${clickedTorso.id}' class='card col-3 p-1 m-2'>
-      <img class='card-img-top' src=${clickedTorso.imageUrl}
+    <div id='${clickedTorso.id}' class='card'>
+      <img class='card-img' src=${clickedTorso.imageUrl}
     </div>`;
   $('#torso-name').html(clickedTorso.name);
   $('#torso').html(domString);
@@ -24,8 +25,8 @@ const writeSelectedTorso = (clickedTorso) => {
 const writeSelectedLeg = (clickedLeg) => {
   let domString = '';
   domString += `
-    <div id='${clickedLeg.id}' class='card col-3 p-1 m-2'>
-      <img class='card-img-top' src=${clickedLeg.imageUrl}
+    <div id='${clickedLeg.id}' class='card'>
+      <img class='card-img' src=${clickedLeg.imageUrl}
     </div>`;
   $('#leg-name').html(clickedLeg.name);
   $('#leg').html(domString);
